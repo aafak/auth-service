48173685c0659108c413f4f5fab47cdc1199797e
Aafak Mohammad <aafak.mitsmca09@gmail.com>
2024-09-14 12:54:47 +0530

    Adding folder structure

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []User{
+	{ID: "1", Name: "user1"},
+	{ID: "2", Name: "user2"},
+	{ID: "3", Name: "user3"},
+}
+
+func getUsers(c *gin.Context) {
+	name := c.Query("name") // query parameter
+	if name != "" {
+		for _, user := range users {
+			if user.Name == name {
+				c.IndentedJSON(http.StatusOK, user)
+				return
+			}
+		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+		return
+	} else {
+		//c.JSON(200, books)
+		c.IndentedJSON(http.StatusOK, users)
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/users", getUsers)
+	// http://localhost:8080/users
+	// http://localhost:8080/users?name=user1
+	router.Run(":8080")
+}
